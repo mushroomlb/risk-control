9020cc113c7af76e5ddaa239d2d25cf40cc69724
lijun17 <lijun17@asiainfo.com>
2019-06-04 11:41:36 +0800

    init

--- src/main/java/com/ai/risk/analysis/lijun/factory/HbaseConnectionFactory.java
+++ src/main/java/com/ai/risk/analysis/lijun/factory/HbaseConnectionFactory.java
@@ -1,20 +1,22 @@
 package com.ai.risk.analysis.lijun.factory;
 
-import com.ai.risk.analysis.lijun.util.HbaseUtil;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+
 import java.io.IOException;
 
 /**
  * 获取hbase连接
+ *
  * @author lijun17
  */
 public class HbaseConnectionFactory {
+
     private static Connection connection = null;
 
-    public static Connection getHbaseConnection(String address){
+    public static Connection getHbaseConnection(String address) {
         Configuration configuration = HBaseConfiguration.create();
         configuration.set("hbase.zookeeper.quorum", address);
         try {
